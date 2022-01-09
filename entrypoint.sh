3224a7dc5feb09dfd3a272e1ea28b5fa98c73a8c
Rolbin <rolbinster@gmail.com>
2022-01-09 19:11:30 +0530

    Update to take from mock build variant

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 hub checkout master
 VERSION_NAME=$(cat ./"${APP_FOLDER}"/version.txt)
-hub release create -a ./"${APP_FOLDER}"/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" v"${VERSION_NAME}"
+hub release create -a ./"${APP_FOLDER}"/build/outputs/apk/mock/*-mock.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" v"${VERSION_NAME}"
